7d70b4ce7bf5135c78878b157ef0cb54b9186860
RanjitSinghChouhan <ranjeet.singh.chouhan94@gmail.com>
2023-05-12 11:38:21 +0530

    removing commented code

--- models/index.js
+++ models/index.js
@@ -1,30 +1,3 @@
-// const fs = require("fs");
-// const path = require("path");
-
-// var db = {};
-// fs.readdirSync(__dirname)
-//   .filter(
-//     (file) =>
-//   {console.log(file);
-//       return file.indexOf(".") !== 0 && file !== "index.js" && file !== "connection.js"
-//   })
-//   .forEach((file) => {
-//     console.log(file, "forexh")
-//     // db = {...db, file}
-//     var model = path.join(__dirname, file);
-//     db[file] = model;
-//   });
-// console.log(db)
-// // Object.keys(db).forEach(function (modelName) {
-// // //   if ("associate" in db[modelName]) {
-// //     {Object.keys(modelName)};
-// // //   }
-// // //   return modelName
-// // console.log(modelName, "name")
-// // });
-// db = {...Object.keys(db).values}
-// console.log(db)
-// module.exports = db;
 const admin = require('./admin')
 
 module.exports={
